d090ec19c4aa49b188c1c5deb45a832c1adb2246
Giuliano <luis.acuna2101@alumnos.ubiobio.cl>
2024-04-12 16:52:13 -0400

    de momento nada, pero lo dejare subido

--- backend/src/models/asignacion.model.js
+++ backend/src/models/asignacion.model.js
@@ -1,1 +1,34 @@
-pruebita cochina
+"use strict";
+
+import { Schema, model } from "mongoose";
+
+const ticketSchema = new Schema(
+  {
+    // Falta conectar con el esquema 'Tarea'
+    id: {
+      type: String,
+      required: true,
+    },
+    asignadoA: {
+      type: String,
+      required: true,
+    },
+    asignadoHistorial: {
+      type: [String],
+      default: function() {
+        return [this.asignadoA];
+      },
+    },
+  },
+  {
+    versionKey: false,
+  },
+);
+
+ticketSchema.methods.agregarAsignacion = function(asignadoA) {
+  this.asignadoHistorial.push(asignadoA);
+};
+
+const Ticket = model("Ticket", ticketSchema);
+
+export default Ticket;
