3919602cb81eefcca3f0d685d9ed661039eca9cc
Fernanda Schonffeldt <f.schonffeldtc@gmail.com>
2024-07-29 22:50:30 -0400

    cambios finales

--- backend/src/constants/facultades.constants.js
+++ backend/src/constants/facultades.constants.js
@@ -1,4 +1,4 @@
 /** Facultades permitidos por la base de datos */
-const FACULTADES = ["Facultad de Ciencias Empresariales", "Facultad de Arquitectura", "Universidad"];
+const FACULTADES = ["Facultad de Ciencias Empresariales", "Facultad de Arquitectura","Facultad de Ciencias", "Universidad"];
 
 export default FACULTADES;
